23ac6f76f84700cb187a043a8c1eb5198a3ecaaf
manipal <manipalreddyt6@gmail.com>
2022-07-23 14:20:25 +0000

    adding passing

--- parameter.sh
+++ parameter.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
-echo "Username: $1";
-echo "Age: $2";
-echo "Full Name: $3";
+visible = $4
+if [ "$visible" = "True"];
+	echo "Username: $1";
+	echo "Age: $2";
+	echo "Full Name: $3";
+else 
+	echo "please pass visible"
+fi
+
